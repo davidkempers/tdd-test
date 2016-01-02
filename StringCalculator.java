b97e86a363a4df8c81f5951b1f97b14021eb60e8
david.kempers@arm.com <david.kempers@arm.com>
2016-01-02 22:28:38 +0000

    Test add empty string refactor

--- StringCalculator.java
+++ StringCalculator.java
@@ -2,6 +2,11 @@
 public class StringCalculator {
 
     public int add(String numbers) {
-        return 0;
+
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        return 1;
     }
 }

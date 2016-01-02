5c90cb3adcbc5ca33855970045257f3c1234e36c
david.kempers@arm.com <david.kempers@arm.com>
2016-01-02 23:41:03 +0000

    Test add multiple delimiter string success

--- StringCalculator.java
+++ StringCalculator.java
@@ -7,7 +7,7 @@ public class StringCalculator {
             return 0;
         }
 
-        String nums[] = numbers.split(",");
+        String nums[] = numbers.split(",|\\\n");
 
         int sum = 0;
         for (int i=0; i < nums.length; i++) {

55c27d6181fb11a1a9efee4085a1520776d9d758
david.kempers@arm.com <david.kempers@arm.com>
2016-01-03 18:23:53 +0000

    Test set delimiter any length refactor and success

--- DelimiterParser.java
+++ DelimiterParser.java
@@ -3,18 +3,58 @@ import java.util.ArrayList;
 public class DelimiterParser {
 
     // default delimiters
-    String[] delimiters = new String[]{",", "\n"};
-    String body = null;
+    private String[] delimiters = new String[]{",", "\n"};
+    private String body = null;
+
+    private enum States {
+        NONE, START, OPEN, CLOSE
+    }
+    private States state  = States.NONE;
 
     public String parse(String str) {
 
-        if (str.startsWith("//") && str.indexOf("\n") > 0) {
-            delimiters = new String[] {str.substring(2, str.indexOf("\n"))};
-            body = str.substring(str.indexOf("\n") + 1);
-        } else {
-            body = str;
+        body = str;
+        String delimiter = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            switch(state) {
+                case NONE:
+                    if (c == '/') {
+                        state = States.START;
+                    } else {
+                        // delimiter not set
+                        return str;
+                    }
+                    break;
+                case START:
+                    if (c == '/') {
+                        state = States.OPEN;
+                    } else {
+                        // invalid
+                    }
+                    break;
+                case OPEN:
+                    if (c == '[') {
+                        // ignore open bracket
+                    } else if (c == ']') {
+                        state = States.CLOSE;
+                    } else if (c == '\n') {
+                        state = States.CLOSE;
+                    } else {
+                        delimiter += c;
+                    }
+                    break;
+                case CLOSE:
+                    // we are finished
+                    delimiters = new String[] {delimiter};
+                    if (c == '\n') {
+                        body = str.substring(i + 1);
+                    } else {
+                        body = str.substring(i);
+                    }
+                    return body;
+            }
         }
-
         return body;
     }
 
@@ -27,23 +67,21 @@ public class DelimiterParser {
         if (body == null) {
             throw new Exception("Delimiters have not been parsed");
         }
-
-        ArrayList<String> result = new ArrayList<>();
-        int offset = 0;
-        for (int i = 0; i < body.length(); i++) {
-            char c = body.charAt(i);
-            for (int j = 0; j < delimiters.length; j++) {
-                if (delimiters[j].indexOf(c) == 0) {
-                    result.add(body.substring(offset, i));
-                    offset = i+1;
-                }
+        String regex = "";
+        for (int j = 0; j < delimiters.length; j++) {
+            String delimiter = "";
+            for (int i = 0; i< delimiters[j].length(); i++) {
+                delimiter += "\\" + delimiters[j].charAt(i);
             }
+            regex = "|" + delimiter;
         }
-        result.add(body.substring(offset));
+        String[] result = body.split(regex.substring(1));
 
-        int[] numbers = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            numbers[i] = Integer.parseInt(result.get(i));
+        int[] numbers = new int[result.length];
+        for (int i = 0; i < result.length; i++) {
+            if (!result[i].isEmpty()) {
+                numbers[i] = Integer.parseInt(result[i]);
+            }
         }
 
         return numbers;

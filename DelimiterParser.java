036531f4611055e456afb522e0ab69f59584f929
david.kempers@arm.com <david.kempers@arm.com>
2016-01-03 00:05:32 +0000

    Test parse delimiter string fail

--- DelimiterParser.java
+++ DelimiterParser.java
@@ -1,3 +1,8 @@
 
 public class DelimiterParser {
+
+    public String[] parse(String str) {
+
+        return new String[]{};
+    }
 }
--- DelimiterParserTest.java
+++ DelimiterParserTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertArrayEquals;
 
 public class DelimiterParserTest {
 
@@ -10,4 +11,13 @@ public class DelimiterParserTest {
         // assert statements
         assertNotNull(delimiters);
     }
+
+    @Test
+    public void testParseDelimiters() {
+
+        DelimiterParser delimiters = new DelimiterParser();
+        // assert statements
+        assertArrayEquals(new String[]{"1","2","3"}, delimiters.parse("1,2\n3"));
+    }
+
 }
